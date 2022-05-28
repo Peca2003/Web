c89d385f757e08e396ff7aa24cae293226906775
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2022-05-28 16:23:43 +0300

    Create js-42-2.js

---
+++ js-42-2.js
@@ -0,0 +1,8 @@
+var year = prompt('Введите число');
+        if (prompt > '0') {
+         alert('1');
+        } else if (prompt < '0') {
+         alert('-1');
+        } else {
+         alert('0');
+        }
