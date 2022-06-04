e27fa6b3ec6adabc8e2493c07c863e9234e23892
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2022-06-04 12:57:29 +0300

    Update js-42-3.js

--- js-42-3.js
+++ js-42-3.js
@@ -1,9 +1,16 @@
 var log = prompt('Введите логин');
     if (log == 'Админ'){
         var pass = prompt('Введите пароль');
-        if (pass == 'Чёрный Властелин') alert('Добро пожаловать!');
-        else if(pass == null) alert('Вход отменён');
-        else alert('Пароль неверен');
+        if (pass == 'Чёрный Властелин'){
+            alert('Добро пожаловать!');
+        }
+        else if(pass == null) {
+            alert('Вход отменён');
+        }
+        else {
+            alert('Пароль неверен');
+        }
+    }else if (log == null){
+        alert('Вход отменён');
     }
-    else if (log == null){ alert('Вход отменён');
-    }else {alert('Я вас не знаю');}
+    else {alert('Я вас не знаю');}
