58c7e918a179fa8e186c332ffe81c87d94c1d623
Peca2003 <90759048+Peca2003@users.noreply.github.com>
2022-06-04 11:54:30 +0300

    Update js-42-2.js

--- js-42-2.js
+++ js-42-2.js
@@ -1,7 +1,7 @@
-var year = prompt('Введите число');
-        if (prompt > '0') {
+var a = prompt('Введите число');
+        if (a > 0) {
          alert('1');
-        } else if (prompt < '0') {
+        } else if (a < 0) {
          alert('-1');
         } else {
          alert('0');
